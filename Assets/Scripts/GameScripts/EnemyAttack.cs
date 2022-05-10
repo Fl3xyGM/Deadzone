662ff229a5280d81fed3b8f6aeefd3bd1d0d6b2d
Paul-surf <fanprodkaqw@hotmail.com>
2022-05-10 16:46:38 +0200

    Zombie attack animation

--- Assets/Scripts/GameScripts/EnemyAttack.cs
+++ Assets/Scripts/GameScripts/EnemyAttack.cs
@@ -7,16 +7,19 @@ public class EnemyAttack : MonoBehaviour {
     
     private bool CanAttack = true;
     private int Cooldown = 0;
-
+    private Animator attackAnimation;
     void Update() {
+        attackAnimation = transform.Find("gfx").GetComponent<Animator>();
         if(!CanAttack) {
             Cooldown++;
+            if (Cooldown == 50) {
+                attackAnimation.SetBool("isAttacking", false);
+            }
             if(Cooldown == 60) {
                 CanAttack = true;
                 Cooldown = 0;
             }
         }
-        
     }
 
     void OnCollisionStay2D(Collision2D col) {
@@ -24,12 +27,8 @@ public class EnemyAttack : MonoBehaviour {
         if(col.collider.tag != "Player"){return;}
         if(CanAttack) {
             col.gameObject.GetComponent<PlayerHealth>().IsAttacked(col.gameObject);
+            attackAnimation.SetBool("isAttacking", true);
             CanAttack = false;
         }
     }
-
-
-
-
-
 }
