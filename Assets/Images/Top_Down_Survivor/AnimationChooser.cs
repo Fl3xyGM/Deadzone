ade7e01aa59128c0f25912e4c0173af5053b3db3
Fl3xyGM <Fl3xyGM@gmail.com>
2022-05-10 20:10:45 +0200

    k

--- Assets/Images/Top_Down_Survivor/AnimationChooser.cs
+++ Assets/Images/Top_Down_Survivor/AnimationChooser.cs
@@ -19,25 +19,26 @@ public class AnimationChooser : NetworkBehaviour {
             DoAnims = true;
             this.GetComponent<Animator>().SetBool("DoAnims", DoAnims);
             PlayerListGame playerListGame = GameObject.Find("PlayerManager").GetComponent<PlayerListGame>();
+            GameObject[] Playerlist = GameObject.FindGameObjectsWithTag("Player");
 
-            if(playerListGame.PlayerList[0] == null) {
-                playerListGame.PlayerList[0].GetComponent<Animator>().runtimeAnimatorController = animatorHolder.Green;
+            if(Playerlist[0] == null) {
+                Playerlist[0].GetComponent<Animator>().runtimeAnimatorController = animatorHolder.Green;
             }
 
-            if(playerListGame.PlayerList.Count > 0 && !IsRun1) {
-                playerListGame.PlayerList[0].GetComponent<Animator>().runtimeAnimatorController = animatorHolder.Green;
+            if(Playerlist.Length > 0 && !IsRun1) {
+                Playerlist[0].GetComponent<Animator>().runtimeAnimatorController = animatorHolder.Green;
                 IsRun1 = true;
             }
-            if(playerListGame.PlayerList.Count > 1 && !IsRun2) {
-                playerListGame.PlayerList[1].GetComponent<Animator>().runtimeAnimatorController = animatorHolder.Blue;
+            if(Playerlist.Length > 1 && !IsRun2) {
+                Playerlist[1].GetComponent<Animator>().runtimeAnimatorController = animatorHolder.Blue;
                 IsRun2 = true;
             }
-            if(playerListGame.PlayerList.Count > 2 && !IsRun3) {
-                playerListGame.PlayerList[2].GetComponent<Animator>().runtimeAnimatorController = animatorHolder.Red;
+            if(Playerlist.Length > 2 && !IsRun3) {
+                Playerlist[2].GetComponent<Animator>().runtimeAnimatorController = animatorHolder.Red;
                 IsRun3 = true;
             }
-            if(playerListGame.PlayerList.Count > 3 && !IsRun4) {
-                playerListGame.PlayerList[3].GetComponent<Animator>().runtimeAnimatorController = animatorHolder.Yellow;
+            if(Playerlist.Length > 3 && !IsRun4) {
+                Playerlist[3].GetComponent<Animator>().runtimeAnimatorController = animatorHolder.Yellow;
                 IsRun4 = true;
             }
 
