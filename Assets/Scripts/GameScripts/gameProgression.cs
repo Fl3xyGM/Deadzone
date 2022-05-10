f2493b79a70a25dfe4945245cc28b1b46cb80302
Paul-surf <fanprodkaqw@hotmail.com>
2022-05-10 17:02:46 +0200

    Added a way to return to main after winning

--- Assets/Scripts/GameScripts/gameProgression.cs
+++ Assets/Scripts/GameScripts/gameProgression.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 using Mirror;
-
 public class gameProgression : NetworkBehaviour {
 
     [SyncVar]
@@ -13,6 +14,7 @@ public class gameProgression : NetworkBehaviour {
     public GameObject WinMenu;
     public bool WinMenuTriggered = false;
     private int MaxSurvivors;
+    private Button returnButton;
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Survivor") {
             if(isServer) {
@@ -21,9 +23,14 @@ public class gameProgression : NetworkBehaviour {
             Destroy(collision.gameObject);
         }
     }
+    void Start() {
+        if(SceneManager.GetActiveScene().name == "TestScene") {
+            returnButton = GameObject.FindGameObjectWithTag("MainCamera").transform.Find("GameWon/Background/ReturnToStart").gameObject.GetComponent<Button>();
+            returnButton.onClick.AddListener(leaveToMainMenu);
+        }
+    }
 
     void Update() {
-
         MaxSurvivors = GameObject.FindGameObjectsWithTag("Player").Length*3;
         
 
@@ -32,6 +39,18 @@ public class gameProgression : NetworkBehaviour {
         if (RescuedCount == MaxSurvivors) {
             WinMenu.SetActive(true);
             WinMenuTriggered = true;
+        }
+        if(Input.GetKey("b")) {
+            RescuedCount += 1;
+        }
+    }
+    void leaveToMainMenu() {
+        Debug.Log("This is a test");
+        if(isServer) {
+            NetworkManager.singleton.StopHost();
+        }
+        if(isClient) {
+            NetworkManager.singleton.StopClient();
         }
     }
 }
